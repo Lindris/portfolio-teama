3d3877c44335199c777956ded4de574075a1e102
Lindris <linda01harris@gmail.com>
2023-05-08 12:36:00 +0100

    layout done and contact me hover

--- portfolio-teama/src/App.js
+++ portfolio-teama/src/App.js
@@ -5,15 +5,30 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src='girl.png' className='girl' alt='girl'></img>
-        <h1>Anna <br></br> Nilm</h1>
+    // <div className="App">
+    <div className='grid-container'>
+      {/* <header className="App-header"> */}
+     
+      <div className='Box1'>
+        <h1 >Anna <br></br> Nilm</h1>
+        </div>
+        <div className='box2'>
+      <img src='girl.png' className="girl box2"alt='girl'></img>
+      </div>
+     
+        {/* <div className="about Box3">About me */}
         <button className="about">About me</button>
+        {/* </div> */}
+       <div className="box4">
         <h3>Aspiring Web Developer</h3>
-        <h3>About Me </h3>
-      </header>
+        </div>
+      <div className="box5">
+        <h3 className='contact'>Contact Me </h3>
+       </div>
+      {/* </header> */}
     </div>
+   
+   
   );
 }
 
