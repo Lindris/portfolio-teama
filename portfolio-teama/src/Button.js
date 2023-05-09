28e2d522cc1b2e621dde0d11867f8e8a0fe59133
Lindris <linda01harris@gmail.com>
2023-05-09 19:35:02 +0100

    css and text content updated

--- portfolio-teama/src/Button.js
+++ portfolio-teama/src/Button.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-
-const Button = ({ message = ''}) => {
- return (
-  <button className='button' type='button'>
-  {message}
-  </button>
- );
+const Button = ({ message = "" }) => {
+  return (
+    <button className="button" type="button">
+      {message}
+    </button>
+  );
 };
 
 export default Button;
--- portfolio-teama/src/Pages/Page2/Page2.js
+++ portfolio-teama/src/Pages/Page2/Page2.js
@@ -10,32 +10,45 @@ function Page2() {
 
       <div className="grid-container">
         <div className="Image Box3">
-          Box3
           <img src="desk.png" className="desk" alt="desk"></img>
         </div>
         <div className="Background Box1">
           <h3 className="Background-heading">Background</h3>
           <div className="Background-list Box1">
-            <p>background paragraph 1</p>
-            <br></br>
-            <p>background paragraph 2</p>
-            <br></br>
-            <p>background paragraph 3</p>
+            <p>Hi, I am Ana Nilm, experienced teacher with a passion for technology. 
+        I am now transitioning into the field of web development.</p>
+            <br></br><br></br>
+            <p>I have acquired a strong foundation in HTML, CSS,
+           and JavaScript through online courses and self-directed learning.</p>
+            <br></br><br></br>
+            <p>With my attention
+           to detail and ability to problem-solve, I am committed to creating clean, user-friendly
+           websites that meet the needs of clients and users. My background in teaching has
+           equipped me with excellent communication and collaboration skills, which enable
+           me to work effectively with teams and clients. I am excited to contribute my unique
+           blend of skills and experience to create innovative web solutions.</p>
             <br></br>
           </div>
         </div>
 
         <div className="Expertise Box2">
 
-          <h3>Expertise</h3>
+          <h3 className='expertise'>Expertise</h3>
           <ul>
-            <li>expertise bullet 2</li><br></br><br></br>
+            <li>Bachelor's degree in Education</li><br></br><br></br>
             <br></br>
-            <li>expertise bullet 3</li><br></br><br></br>
+            <li>Code First Girls Bootcamp - Intro into Javascript</li><br></br><br></br>
             <br></br>
-            <li>expertise bullet 2</li><br></br><br></br>
+            <li>Certificate in Web Development from an accredited online program
+Completed a 12-week Full-Stack Web Development Bootcamp with a focus on JavaScript, React, and Node.js</li><br></br><br></br>
             <br></br>
-            <li>expertise bullet 3</li><br></br><br></br>
+            <li>Completed online courses in HTML, CSS, JavaScript, and web design from reputable providers such as Udemy and Codecademy</li><br></br><br></br>
+            <br></br>
+
+
+            <li>Proficient in using design tools such as Figma and Adobe XD for prototyping and mockups</li><br></br><br></br>
+            <br></br>
+            <li>Familiarity with Agile development methodologies and project management tools such as Trello and Asana.</li><br></br><br></br>
             <br></br>
           </ul>
         </div>
